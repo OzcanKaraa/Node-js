b5410ee8a369821ae1bef34856f1838853a7dc7e
Özcan Kara <ozcankara810@gmail.com>
2023-09-19 03:03:01 +0300

    some changes

--- 1.Node.js_Kavramlar/app1.js
+++ 1.Node.js_Kavramlar/app1.js
@@ -2,7 +2,7 @@ console.log("Hello World");
 var name = "Ozcan Kara";
 
 //Global Obje
-//console.log(window);  Window objesi ile farkli dosyalardan da calistirilabilir.
+console.log(window);  // Window objesi ile farkli dosyalardan da calistirilabilir.
 
 var firstName = 'Ozcan';
 console.log(firstName);
@@ -12,7 +12,6 @@ console.log(window);
 var firstName = 'Ozcan';
 console.log(firstName);
 console.log(window);
-
 
 //GLOBAL OBJE
 var lastName = 'Kara';
