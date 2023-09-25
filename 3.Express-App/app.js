5c1510009c8398ced1bfb5075192a4696c9ab1f4
Özcan Kara <ozcankara810@gmail.com>
2023-09-25 04:08:25 +0300

    some changes

---
+++ 3.Express-App/app.js
@@ -0,0 +1,42 @@
+//Express Router
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/user');
+app.use(bodyParser.urlencoded({extended:false}));
+const path = require('path');
+
+//routes
+app.use('admin',adminRoutes);
+app.use('admin',userRoutes);
+
+//Error Sayfasi (404)  Html Sayfa Gönderimi
+app.use((req,res)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+  //Error Sayfasi (404) 
+/*app.use((req,res)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+   //    res.status(404);
+   //   res.send('<h1>Page not found</h1>');
+});
+*/
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
+const express = require('express');
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+app.get('/api/products',(req,res) =>{
+    res.send('urunler listelendi');
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
--- 3.Express-App/app1.js
+++ 3.Express-App/app1.js
@@ -1,22 +1,7 @@
-const express = require('express');
-const app = express();
-
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-});
-
-app.get('/api/products',(req,res) =>{
-    res.send('urunler listelendi');
-});
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-});
---------------------------------------------------------------------------------------------------
 /*
 Middleware : Codding fonksiyonu parametre alarak (request,response,next) parametreleri geriye response dondurme.
 Next fonksiyonu ile middleware gecisleri saglanir.Request response surecleri gerceklesir.Response ile sureci sonlandirilabilir.
-*/
+
 const express = require('express');
 const app = express();
 
@@ -49,7 +34,7 @@ app.use('/add-product',(req,res,next)=>{
     res.send('<h1>adding product page </h1>');
 });
 */
-
+/*
 app.use('/product-list',(req,res,next)=>{
     res.send('<h1>Product Listing page </h1>');
 });
@@ -64,7 +49,7 @@ app.listen(3000,()=>{
 
 --------------------------------------------------------------------------------------------------
 //Body Parser
-
+/*
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -75,6 +60,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 });
 
 //Sadece Get metod request filtreleme
+/*
 app.get('/add-product',(req,res,next)=>{
     res.send(`
     <html>
@@ -89,7 +75,7 @@ app.get('/add-product',(req,res,next)=>{
     `);
 });
 
-/*
+
 app.use('/product-list',(req,res,next)=>{
     res.send(`
     <html>
@@ -111,7 +97,7 @@ app.use('/product-list',(req,res,next)=>{
     console.log(req.body); //database kayit
     res.redirect('/');
 });
-*/
+
 
 //Sadece Post Metodu Request
 app.post('/product',(req,res,next)=>{
@@ -127,33 +113,4 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
 
---------------------------------------------------------------------------------------------------
-//Express Router
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
-app.use(bodyParser.urlencoded({extended:false}));
-const path = require('path');
-//routes
-app.use('admin',adminRoutes);
-app.use('admin',userRoutes);
-
-//Error Sayfasi (404)  Html Sayfa Gönderimi
-app.use((req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-});
-
-  //Error Sayfasi (404) 
-/*app.use((req,res)=>{
-    res.status(404).send('<h1>Page not found</h1>');
-   //    res.status(404);
-   //   res.send('<h1>Page not found</h1>');
-});
 */
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-});
